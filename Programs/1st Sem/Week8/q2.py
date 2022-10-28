c30620fc853e3fb8aa1e0ad5af19e8027593f74a
pk <premkumarsdps@mail.com>
2022-10-28 21:27:40 +0530

    pull request
    week 8

--- Programs/1st Sem/Week8/q2.py
+++ Programs/1st Sem/Week8/q2.py
@@ -1,8 +1,8 @@
-def rev(st):
-    if len(st)==0:
+def rev(st):#defining function
+    if len(st)==0:#if coditon to check the length of the string 
         return st
     else:
-        return rev(st[1:])+st[0]
+        return rev(st[1:])+st[0]#reverses the string 
 
 st=input('Enter a string to reverse \n')
-print(rev(st))
+print(rev(st))#calling function 
--- Programs/1st Sem/Week8/q3.py
+++ Programs/1st Sem/Week8/q3.py
@@ -1,9 +1,9 @@
 #tower of hanoi
-def toh(n,from1,to,aux):
+def toh(n,from1,to,aux):#initializing function 
     if n==1:
-        print('Move ring 1 from',from1,'peg to',to,'peg')
+        print('Move ring 1 from',from1,'peg to',to,'peg')#base condition
         return
-    toh(n-1,from1,aux,to)
+    toh(n-1,from1,aux,to)#recursive function
     print('Move ring',n,'from',from1,'peg to',to,'peg')
     toh(n-1,aux,to,from1)
 
@@ -15,7 +15,7 @@ def powe(n,b1):
     if n==1:
         return b1
     else:
-        return powe(n-1,b1*b)
+        return powe(n-1,b1*b)#returns th number b raised to the power of n
 b=int(input('Enter the base\n'))
 n=int(input('Enter the power to which it is to be raised\n'))
 print(powe(n,b))
