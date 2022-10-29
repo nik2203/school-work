bc5e587065b247e6652567b9e102356c8ad0e1da
pk <premkumarsdps@mail.com>
2022-10-30 01:29:51 +0530

    Revert "Revert "pull request""
    This reverts commit 812525ce2b2310b2a4e2a97056b6a385c7cca6dc.

--- Programs/1st Sem/Week8/q1.py
+++ Programs/1st Sem/Week8/q1.py
@@ -1,10 +1,10 @@
-def seive(n):
-    comp_no=[]
-    for i in range(2,n+1):
-        if i not in comp_no:
+def seive(n):#DEFINING FUNCTION SEIVE
+    comp_no=[]#INITIALIZING EMPTY LIST
+    for i in range(2,n+1):#FOR LOOP 
+        if i not in comp_no:#INITIALIZING IF CONDITION TO ADD TO LIST 
             print(i)
             for j in range(i*i,n+1,i):
-                comp_no.append(j)
+                comp_no.append(j)#APPEND PRIME NUMBERS
 
 n=int(input('Enter the range to find prime numbers\n'))
-seive(n)
+seive(n)#CALLING FUNCTION
